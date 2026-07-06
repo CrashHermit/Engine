3b2be645594a0691a2f3d4adf334af2a0fea1315
Joshua Hicks <crashhermit@gmail.com>
2026-07-05 22:43:52 -0500

    Mesh completed? rewritten

--- src/core/geometry/mesh.py
+++ src/core/geometry/mesh.py
@@ -1,178 +1,155 @@
-"""Geodesic icosphere mesh generation."""
-
 import numpy as np
+
+PHI: float = (1.0 + np.sqrt(5.0)) / 2.0
+
+BASE_VERTICES: np.ndarray = np.array(
+    [
+        [-1, PHI, 0],
+        [1, PHI, 0],
+        [-1, -PHI, 0],
+        [1, -PHI, 0],
+        [0, -1, PHI],
+        [0, 1, PHI],
+        [0, -1, -PHI],
+        [0, 1, -PHI],
+        [PHI, 0, -1],
+        [PHI, 0, 1],
+        [-PHI, 0, -1],
+        [-PHI, 0, 1],
+    ]
+)
+
+BASE_FACES: np.ndarray = np.array(
+    [
+        [0, 5, 1],
+        [0, 1, 7],
+        [0, 7, 10],
+        [0, 10, 11],
+        [0, 11, 5],
+        [5, 11, 4],
+        [11, 10, 2],
+        [10, 7, 6],
+        [7, 1, 8],
+        [3, 4, 2],
+        [3, 2, 6],
+        [3, 6, 8],
+        [3, 8, 9],
+        [3, 9, 4],
+        [2, 4, 11],
+        [6, 2, 10],
+        [8, 6, 7],
+        [9, 8, 1],
+        [1, 9, 5],
+        [5, 9, 4],
+    ]
+)
+
+
+def generate_icosphere(nu: int) -> tuple[np.ndarray, np.ndarray]:
+    vertex_map: dict[tuple[float, float, float], int] = {}
+    raw_vertices: list[np.ndarray] = []
+    raw_faces: list[list[int]] = []
+
+    for face in BASE_FACES:
+        vertex_a: np.ndarray = BASE_VERTICES[face[0]]
+        vertex_b: np.ndarray = BASE_VERTICES[face[1]]
+        vertex_c: np.ndarray = BASE_VERTICES[face[2]]
+
+        grid: dict[tuple[int, int], int] = {}
+
+        for i in range(nu + 1):
+            for j in range(nu + 1 - i):
+                barycentric_weight: int = nu - i - j
+                raw_position: np.ndarray = ((i * vertex_a) + (j * vertex_b) + (barycentric_weight * vertex_c)) / nu
+                normalized_position: np.ndarray = raw_position / np.linalg.norm(raw_position)
+                rounded_key: tuple[float, float, float] = tuple(np.round(normalized_position, 6))
+
+                if rounded_key not in vertex_map:
+                    vertex_map[rounded_key] = len(raw_vertices)
+                    raw_vertices.append(raw_position)
+
+                grid[(i, j)] = vertex_map[rounded_key]
+
+        for i in range(nu):
+            for j in range(nu - i):
+                raw_faces.append([grid[(i, j)], grid[(i + 1, j)], grid[(i, j + 1)]])
+                if j < nu - i - 1:
+                    raw_faces.append([grid[(i + 1, j)], grid[(i + 1, j + 1)], grid[(i, j + 1)]])
+
+    vertices: np.ndarray = np.array(raw_vertices)
+    vertices /= np.linalg.norm(vertices, axis=1, keepdims=True)
+    faces: np.ndarray = np.array(raw_faces)
+
+    return vertices, faces
 
 
 class Mesh:
-    def __init__(self, nu: int, radius: float) -> None:
-        self.nu: int = nu
-        self.radius: float = radius
+    def __init__(self, vertices: np.ndarray, faces: np.ndarray) -> None:
+        self.vertices: np.ndarray = vertices
+        self.faces: np.ndarray = faces
 
-        self._vertex_map: dict[tuple[float, float, float], int] = {}
-        self._raw_vertices: list[np.ndarray] = []
-        self._raw_faces: list[list[int]] = []
+        self._neighbors: list[list[int]] | None = None
+        self._dual_vertices: np.ndarray | None = None
+        self._dual_faces: list[list[int]] | None = None
 
-        self._generate_frequency_icosphere()
+    @property
+    def neighbors(self) -> list[list[int]]:
+        if self._neighbors is None:
+            num_vertices: int = len(self.vertices)
+            neighbor_sets: list[set[int]] = [set() for _ in range(num_vertices)]
 
-    def _add_vertex(self, vertex_coordinates: np.ndarray) -> int:
-        normalized_vertex = vertex_coordinates / np.linalg.norm(vertex_coordinates)
-        key = tuple(np.round(normalized_vertex, 6))
+            for face in self.faces:
+                vertex_a: int = int(face[0])
+                vertex_b: int = int(face[1])
+                vertex_c: int = int(face[2])
 
-        if key not in self._vertex_map:
-            self._vertex_map[key] = len(self._raw_vertices)
-            self._raw_vertices.append(normalized_vertex * self.radius)
+                neighbor_sets[vertex_a].add(vertex_b)
+                neighbor_sets[vertex_a].add(vertex_c)
+                neighbor_sets[vertex_b].add(vertex_a)
+                neighbor_sets[vertex_b].add(vertex_c)
+                neighbor_sets[vertex_c].add(vertex_a)
+                neighbor_sets[vertex_c].add(vertex_b)
 
-        return self._vertex_map[key]
+            self._neighbors = [sorted(neighbor_set) for neighbor_set in neighbor_sets]
 
-    @staticmethod
-    def _get_base_icosahedron() -> tuple[np.ndarray, np.ndarray]:
-        phi = (1.0 + np.sqrt(5.0)) / 2.0
+        return self._neighbors
 
-        base_vertices = np.array(
-            [
-                [-1, phi, 0],
-                [1, phi, 0],
-                [-1, -phi, 0],
-                [1, -phi, 0],
-                [0, -1, phi],
-                [0, 1, phi],
-                [0, -1, -phi],
-                [0, 1, -phi],
-                [phi, 0, -1],
-                [phi, 0, 1],
-                [-phi, 0, -1],
-                [-phi, 0, 1],
-            ]
-        )
+    @property
+    def dual_vertices(self) -> np.ndarray:
+        if self._dual_vertices is None:
+            self._dual_vertices = self.vertices[self.faces].mean(axis=1)
+        return self._dual_vertices
 
-        base_faces = np.array(
-            [
-                [0, 5, 1],
-                [0, 1, 7],
-                [0, 7, 10],
-                [0, 10, 11],
-                [0, 11, 5],
-                [5, 11, 4],
-                [11, 10, 2],
-                [10, 7, 6],
-                [7, 1, 8],
-                [3, 4, 2],
-                [3, 2, 6],
-                [3, 6, 8],
-                [3, 8, 9],
-                [3, 9, 4],
-                [2, 4, 11],
-                [6, 2, 10],
-                [8, 6, 7],
-                [9, 8, 1],
-                [1, 9, 5],
-                [5, 9, 4],
-            ]
-        )
+    @property
+    def dual_faces(self) -> list[list[int]]:
+        if self._dual_faces is None:
+            centroids: np.ndarray = self.dual_vertices
+            num_vertices: int = len(self.vertices)
 
-        return base_vertices, base_faces
+            vertex_faces: list[list[int]] = [[] for _ in range(num_vertices)]
+            for face_index, face in enumerate(self.faces):
+                for vertex in face:
+                    vertex_faces[int(vertex)].append(face_index)
 
-    def _subdivide_face_grid(
-        self, face: np.ndarray, base_vertices: np.ndarray
-    ) -> dict[tuple[int, int], int]:
-        a = base_vertices[face[0]]
-        b = base_vertices[face[1]]
-        c = base_vertices[face[2]]
+            dual: list[list[int]] = []
+            for vertex_index in range(num_vertices):
+                face_indices: list[int] = vertex_faces[vertex_index]
+                position: np.ndarray = self.vertices[vertex_index]
+                centroid_vectors: np.ndarray = centroids[face_indices] - position
 
-        nu = self.nu
-        grid: dict[tuple[int, int], int] = {}
-        for i in range(nu + 1):
-            for j in range(nu + 1 - i):
-                k: int = nu - i - j
-                vertex_coordinates: np.ndarray = ((i * a) + (j * b) + (k * c)) / nu
-                grid[(i, j)] = self._add_vertex(vertex_coordinates=vertex_coordinates)
-        return grid
+                reference_vector: np.ndarray = centroid_vectors[0] / np.linalg.norm(centroid_vectors[0])
+                tangent_u: np.ndarray = reference_vector
+                tangent_v: np.ndarray = np.cross(position, tangent_u)
+                tangent_v = tangent_v / np.linalg.norm(tangent_v)
+                tangent_u = np.cross(tangent_v, position)
 
-    def _stitch_face_triangles(self, grid: dict[tuple[int, int], int]) -> None:
-        nu = self.nu
-        for i in range(nu):
-            for j in range(nu - i):
-                self._raw_faces.append(
-                    [grid[(i, j)], grid[(i + 1, j)], grid[(i, j + 1)]]
-                )
+                tangent_u_component: np.ndarray = np.dot(centroid_vectors, tangent_u)
+                tangent_v_component: np.ndarray = np.dot(centroid_vectors, tangent_v)
+                angular_order: np.ndarray = np.argsort(np.arctan2(tangent_v_component, tangent_u_component))
 
-                if j < nu - i - 1:
-                    self._raw_faces.append(
-                        [grid[(i + 1, j)], grid[(i + 1, j + 1)], grid[(i, j + 1)]]
-                    )
+                dual.append([face_indices[i] for i in angular_order])
 
-    def _build_adjacency(self) -> list[list[int]]:
-        num_vertices = len(self.vertices)
-        neighbors: list[set[int]] = [set() for _ in range(num_vertices)]
+            self._dual_faces = dual
+        return self._dual_faces
 
-        for face in self.faces:
-            v0, v1, v2 = int(face[0]), int(face[1]), int(face[2])
-            neighbors[v0].add(v1)
-            neighbors[v0].add(v2)
-            neighbors[v1].add(v0)
-            neighbors[v1].add(v2)
-            neighbors[v2].add(v0)
-            neighbors[v2].add(v1)
 
-        return [sorted(nbr_set) for nbr_set in neighbors]
-
-    def _build_face_centroids(self) -> np.ndarray:
-        """Centroid of every triangle face, shape ``(n_faces, 3)``."""
-        return self.vertices[self.faces].mean(axis=1)
-
-    def _build_vertex_faces(self) -> list[list[int]]:
-        """For each vertex, the face indices that contain it."""
-        n_vertices = len(self.vertices)
-        vertex_faces: list[list[int]] = [[] for _ in range(n_vertices)]
-
-        for face_idx, face in enumerate(self.faces):
-            for v in face:
-                vertex_faces[int(v)].append(face_idx)
-
-        return vertex_faces
-
-    def _sort_vertex_faces(self, vertex_idx: int, face_indices: list[int]) -> list[int]:
-        """Sort face indices by the angular order of their centroids around a vertex."""
-        pos = self.vertices[vertex_idx]
-        centroids = self.dual_vertices[face_indices]
-        vecs = centroids - pos
-
-        ref = vecs[0] / np.linalg.norm(vecs[0])
-
-        tangent_u = ref
-        tangent_v = np.cross(pos, tangent_u)
-        tangent_v = tangent_v / np.linalg.norm(tangent_v)
-        tangent_u = np.cross(tangent_v, pos)
-
-        u = np.dot(vecs, tangent_u)
-        v = np.dot(vecs, tangent_v)
-        order = np.argsort(np.arctan2(v, u))
-
-        return [face_indices[i] for i in order]
-
-    def _build_dual_mesh(self) -> None:
-        """Build dual mesh: hexagons and pentagons from the triangular icosphere."""
-        self.dual_vertices = self._build_face_centroids()
-        vertex_faces = self._build_vertex_faces()
-
-        self.dual_faces: list[list[int]] = []
-
-        for v in range(len(self.vertices)):
-            sorted_indices = self._sort_vertex_faces(v, vertex_faces[v])
-            self.dual_faces.append(sorted_indices)
-
-    def _generate_frequency_icosphere(self) -> None:
-        base_vertices, base_faces = self._get_base_icosahedron()
-
-        for face in base_faces:
-            grid = self._subdivide_face_grid(face, base_vertices)
-            self._stitch_face_triangles(grid)
-
-        self.vertices = np.array(self._raw_vertices)
-        self.faces = np.array(self._raw_faces)
-        self.neighbors = self._build_adjacency()
-        self._build_dual_mesh()
-
-        self._vertex_map = {}
-        self._raw_vertices = []
-        self._raw_faces = []
